1ab2ef9127d6daa44cc039230f621e033230b0a7
machariamwangi <elijahmacharia54@gmail.com>
2020-07-29 00:02:01 +0300

    positional arguments in functions

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-def greet_user():
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}!")
     print('Hi there!')
     print('Welcome aboard')
 
 
 print("Start")
-greet_user()
+greet_user("Elijah", "mash")
+greet_user("Mary", "Mwega")
 print("Finish")
